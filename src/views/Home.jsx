904c11641c5962ca60992799b858604daeaae2d6
Zekeyy <146742405+Zekeyy@users.noreply.github.com>
2025-07-13 04:28:18 -0700

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -203,7 +203,7 @@ function Home() {
                                     }
                                 >
                                     Apply
-                                <Link>
+                                </Link>
 
                                 <div className="w-full block text-center bg-green-800 sm:border-2 sm:border-white hover:bg-white hover:text-green-950 hover:border-2 hover:border-green-950 md:hover:opacity-100 transition text-white rounded-md hover:opacity-80 active:opacity-60 cursor-pointer">
                                     <Link
